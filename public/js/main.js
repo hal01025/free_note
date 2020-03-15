785acd750259dbf34f58ddb5064a506dc1c3354a
takumi.okamoto <halOT01025@gmail.com>
2020-03-15 23:38:23 +0000

    modify scroll

--- public/js/main.js
+++ public/js/main.js
@@ -5,10 +5,19 @@ $delete.on('click', () => {
 });
 
 $(window).on('load', () => {
+    $('.scroll').hide();
     $('html, body').animate({ scrollTop: $('.image-gallery').offset().top }, 1500);
 });
 
 $scroll.on('click', (e) => {
     e.preventDefault();
     $('html, body').animate({ scrollTop: 0 }, 600);
+});
+
+$(window).on('scroll', () => {
+    if($(window).scrollTop() >= 300) {
+        $('.scroll').fadeIn();
+    } else {
+        $('.scroll').fadeOut();
+    }
 });
